56eb8ace7848e838c882d102e40e27efc62f1084
Nino Pereira <ninopereira@users.noreply.github.com>
2017-07-07 23:39:30 +0100

    expand has symbols showing direction of expansion
    This is not yet the final answer. We still have to clean the table by removing the expansions that are not leading to the goal.

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -3,6 +3,25 @@
 # keeps track of which step each node was
 # expanded.
 # from Udacity Self-Driving Car Nanodegree course
+# -----------
+# User Instructions:
+#
+# Modify the the search function so that it returns
+# a shortest path as follows:
+# 
+# [['>', 'v', ' ', ' ', ' ', ' '],
+#  [' ', '>', '>', '>', '>', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', 'v'],
+#  [' ', ' ', ' ', ' ', ' ', '*']]
+#
+# Where '>', '<', '^', and 'v' refer to right, left, 
+# up, and down motions. Note that the 'v' should be 
+# lowercase. '*' should mark the goal cell.
+#
+# You may assume that all test cases for this function
+# will have a path from init to goal.
+# ----------
 
 grid = [[0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 0, 0],
@@ -13,10 +32,10 @@ init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
 
-delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
@@ -26,8 +45,8 @@ def search(grid,init,goal,cost):
     # ----------------------------------------
     closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
     closed[init[0]][init[1]] = 1
-    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
-    count = 0
+    expand = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
+
     x = init[0]
     y = init[1]
     g = 0
@@ -40,26 +59,32 @@ def search(grid,init,goal,cost):
     while not found and not resign:
         if len(open) == 0:
             resign = True
+            return 'fail'
         else:
             open.sort()
             open.reverse()
             next = open.pop()
-            row = next[1]
-            col = next[2]
+            x = next[1]
+            y = next[2]
             g = next[0]
-            expand[row][col] = count;
-            count = count + 1;
-            if row == goal[0] and col == goal[1]:
+            
+            if x == goal[0] and y == goal[1]:
                 found = True
+                expand[x][y] = '*'
             else:
                 for i in range(len(delta)):
-                    x2 = row + delta[i][0]
-                    y2 = col + delta[i][1]
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            expand[x][y] = delta_name[i];
                             g2 = g + cost
                             open.append([g2, x2, y2])
                             closed[x2][y2] = 1
-    return expand
+    
+    return expand # make sure you return the shortest path
 
-print search(grid,init,goal,cost)
+result = search(grid,init,goal,cost)
+
+for elem in result:
+    print elem
