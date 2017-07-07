1908a3c015780af5cc09d65c20ebcb10ae271bbc
Nino Pereira <ninopereira@users.noreply.github.com>
2017-07-07 23:23:54 +0100

    Search returns a table tracking expanded nodes
    Search function now returns a table of values called expand. This table keeps track of which step each node was expanded.

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,20 +1,14 @@
-# ----------
-# search() returns a list
-# in the form of [optimal path length, row, col]. For
-# the grid shown below, the function should output
-# [11, 4, 5].
-#
-# If there is no valid path from the start point
-# to the goal, the function should return the string
-# 'fail'
-# ----------
+# search returns
+# a table of values called expand. This table
+# keeps track of which step each node was
+# expanded.
+# from Udacity Self-Driving Car Nanodegree course
 
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
-
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
@@ -26,60 +20,46 @@ delta = [[-1, 0], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-def get_smallest(open):
-  smallest_index = 0;
-  smallest_value = open[0][0];
-  for i in range(len(open)):
-    if open[i][0]<smallest_value:
-      smallest_value = open[i][0]
-      smallest_index = i
-  return smallest_index
-
-def mark_visited(node,visited_grid):
-  visited_grid[node[1]][node[2]] = 1
-  return visited_grid
-
-def expand_grid(node_to_expand,visited_grid,delta,cost):
-  row = node_to_expand[1]
-  col = node_to_expand[2]
-  new_nodes = []
-  for change in delta:
-
-    if (row+change[0])>=0 and (row+change[0])<len(visited_grid):
-      if (col+change[1])>=0 and (col+change[1])<len(visited_grid[row+change[0]]):
-        if visited_grid[row+change[0]][col+change[1]] == 0:
-          new_node = [node_to_expand[0]+cost,row+change[0],col+change[1]]
-          new_nodes.append(new_node)
-          visited_grid = mark_visited(new_node,visited_grid)
-  return [new_nodes,visited_grid]
-
-def check_goal(new_nodes,goal):
-  goal_reached = 0
-  path = []
-  for node in new_nodes:
-    if ((node[1] == goal[0]) and (node[2] == goal[1])):
-      goal_reached = 1;
-      path = node
-  return [goal_reached, path]
-
-
 def search(grid,init,goal,cost):
-    visited_grid = list(grid)
+    # ----------------------------------------
+    # modify code below
+    # ----------------------------------------
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    count = 0
+    x = init[0]
+    y = init[1]
+    g = 0
 
-    open = []
-    open.append([0,init[0],init[1]])
-    while (open):
-        smallest = get_smallest(open)
-        node_to_expand = open[smallest]
-        open.remove(node_to_expand)
-        visited_grid = mark_visited(node_to_expand,visited_grid)
-        [new_nodes,visited_grid] = expand_grid(node_to_expand,visited_grid,delta,cost)
-        open = open + new_nodes
-        [goal_reached, path] = check_goal(new_nodes,goal)
-        if (goal_reached):
-          return path
-        if len(open)==0:
-          break;
-    return 'fail'
+    open = [[g, x, y]]
 
-print (search(grid,init,goal,cost))
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            row = next[1]
+            col = next[2]
+            g = next[0]
+            expand[row][col] = count;
+            count = count + 1;
+            if row == goal[0] and col == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = row + delta[i][0]
+                    y2 = col + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+    return expand
+
+print search(grid,init,goal,cost)
